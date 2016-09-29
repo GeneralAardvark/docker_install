510267b38ba871970ac933575507c414ea23b9ba
GeneralAardvark <generalaardvark@piesweb.co.uk>
2016-09-29 10:51:12 +0100

    docker install script

---
+++ docker_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+USER=$1
+
+apt-get update
+apt-get -y install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get -y purge lxc-docker
+apt-get -y install linux-image-extra-$(uname -r) docker-engine
+service docker start
+groupadd docker
+usermod -aG docker $USER
+systemctl enable docker
+
